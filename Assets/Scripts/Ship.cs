afe2e259c32377c0d225d35ab039c817b1bed22c
Davis Odom <me@davisodom.com>
2019-05-08 15:15:26 -0400

    Ships can be dragged now

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Ship : MonoBehaviour
+public class Ship : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    public static GameObject shipBeingDragged;
+    Vector3 startPos;
     public List<GameObject> cells = new List<GameObject>();
-    //public bool activated = false;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject fleet;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Ship.shipBeingDragged = gameObject;
+        startPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Ship.shipBeingDragged = null;
+        transform.position = startPos;
     }
 }
